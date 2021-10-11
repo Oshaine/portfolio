1cd7dace695452a6c6a19f112e87b51fd1dbabd8
Oshaine <oshaine429smith@gmail.com>
2021-10-10 21:09:00 -0500

    updated

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,9 +1,9 @@
 <template>
-<div class="player">
+<div class="player" id="beats">
        <div class="heading white">
       <h2>Beats</h2>
     </div>
-        <section  id="beats">
+        <section  >
         <iframe src="https://player.beatstars.com/?storeId=97742" width="100%" height="800"> -- none -- </iframe>
     </section>
 </div>
