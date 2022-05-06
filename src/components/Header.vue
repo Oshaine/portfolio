7d0504c3e06afcc3a2087480d109d8985ce1cc79
Oshaine <oshaine429smith@gmail.com>
2022-05-05 22:32:09 -0500

    resume updates

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
       <div class="text-banner">
         <h2>Hello, I'm <br /><span>Oshaine X Smith</span></h2>
         <h3>An Aspiring Web Developer and Music Producer</h3>
-        <a href="/Oshaine_Smith_Resume.pdf" class="btn">Resume</a>
+        <a href="https://drive.google.com/file/d/1ceVvvz-qE9C543Kt_-ZdzaotB9krgMmY/view?usp=sharing" class="btn">Resume</a>
       </div>
     </section>
   </div>
